0d40dcee40adef7043f7d600431d9689e0c7a943
CYRIX-CCS <akinimbomnkwi@gmail.com>
2023-07-11 00:56:10 +0100

    adding comments

--- sum-of-two-numbers/sum.cpp
+++ sum-of-two-numbers/sum.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main () {
 	int x,y;
 	
-	cout << "Enter number 1: ";
+	cout << "Enter number 1: "; //prompting the user to input the first number
 	cin >> x;
-	cout << "Enter number 2: ";
+	cout << "Enter number 2: "; //prompting the user to input the first number
 	cin >> y;
 	
-	int sum = x + y;
+	int sum = x + y; // storing the sum of the two numbers in var 'sum'
 	cout << "The sum of "<<x<<" and "<<y<< " is "<< sum;
 	
 	return 0;
