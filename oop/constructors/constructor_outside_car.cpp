30119e1370c1390ce3d6e476f778f328177b0be5
Nkwi Cyril Akinimbom <akinimbomnkwi@gmail.com>
2023-08-16 19:51:48 +0100

    added another argument to car constructor

--- oop/constructors/constructor_outside_car.cpp
+++ oop/constructors/constructor_outside_car.cpp
@@ -4,25 +4,29 @@ using namespace std;
 
 class car{
     public:
+        string name;
         string brand;
         string model;
         int year;
 
-        car(string x, string y, int z);
+        car(string w, string x, string y, int z);
 };
 
-car::car(string x, string y, int z) {
+car::car(string w, string x, string y, int z) {
+    name = w;
     brand = x;
     model = y;
     year = z;
-            
+
+    cout << "\t" << name << endl;
     cout << "Car Brand: " << x << endl;
     cout << "Car Model: " << y << endl;
-    cout << "Year of Production: " << z ;
+    cout << "Year of Production: " << z << "\n\n";
 }
 
 int main() {
-    car Car1("Mercedes-Benz","G-Wagon",2019);
+    car Car1("Car1","Mercedes-Benz","G-Wagon",2019);
+    car Car2("Car2", "Toyota", "Tacoma", 2017);
 	    
     return 0;
 }
