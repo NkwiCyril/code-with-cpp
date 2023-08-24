20bbd0690cb7b3e5263f66f58b9b38f956d83f44
Nkwi Cyril Akinimbom <akinimbomnkwi@gmail.com>
2023-08-24 14:00:12 +0100

    multilevel inheritance

--- oop/inheritance/vehicle.cpp
+++ oop/inheritance/vehicle.cpp
@@ -13,13 +13,24 @@ class Vehicle {
 
 class Car:public Vehicle {
     public:
+        int year = 2014;
         string model = "Aventador";
+};
+
+// there is another level of inheritance known as multilevel inheritance and here, a class 
+// (grand-child) will be inheriting from the child class instead of the parent class
+
+class SubCar:public Car {
 };
 
 int main() {
     Car car;
+    Subcar c1;
     cout << "Brand: " << car.brand << endl;
-    cout << "Model: " << car.model;
+    cout << "Model: " << car.model << endl << endl;
 
+    cout << "Year of Production: " << c1.year << endl;
+    cout << "Brand: " << c1.brand;
+    
     return 0;
 }
